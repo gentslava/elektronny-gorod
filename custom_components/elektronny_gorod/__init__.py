3320d8cc3ece6d0438df462d3d4326d945999cce
Vyacheslav Scherbinin <gentslava@mail.ru>
2023-12-12 14:34:08 +0700

    Coordinator import fix

--- custom_components/elektronny_gorod/__init__.py
+++ custom_components/elektronny_gorod/__init__.py
@@ -6,6 +6,7 @@ from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
+from .coordinator import ElektronnyGorogDataUpdateCoordinator
 
 # TODO List the platforms that you want to support.
 # For your initial PR, limit it to 1 platform.
@@ -16,7 +17,7 @@ PLATFORMS: list[Platform] = [
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Elektronny Gorod from a config entry."""
-    coordinator = DreameVacuumDataUpdateCoordinator(hass, entry=entry)
+    coordinator = ElektronnyGorogDataUpdateCoordinator(hass, entry=entry)
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
