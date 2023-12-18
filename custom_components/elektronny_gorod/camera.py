3ca6d4ace6485fb5b8892bf56dd5be17403ef882
Vyacheslav Scherbinin <gentslava@mail.ru>
2023-12-19 02:58:32 +0700

    Integration loading improvement

--- custom_components/elektronny_gorod/camera.py
+++ custom_components/elektronny_gorod/camera.py
@@ -3,12 +3,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import (
-  DOMAIN,
-  LOGGER,
-  CONF_WIDTH,
-  CONF_HEIGHT
-)
+from .const import CONF_HEIGHT, CONF_WIDTH, DOMAIN, LOGGER
 from .coordinator import ElektronnyGorogDataUpdateCoordinator
 
 async def async_setup_entry(
@@ -20,39 +15,33 @@ async def async_setup_entry(
     coordinator: ElektronnyGorogDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
 
     # Get cameras info
-    cameras = await coordinator.get_cameras_info()
+    cameras_info = await coordinator.get_cameras_info()
 
     # Create camera entities
-    entities = []
-    for camera in cameras:
-        stream_url = await coordinator.get_camera_stream(camera["ID"])
-        entities.append(
-            ElektronnyGorogCamera(
-                coordinator,
-                camera,
-                stream_url
-            )
-        )
-    async_add_entities(entities)
+    async_add_entities(
+        ElektronnyGorogCamera(
+            coordinator,
+            camera_info
+        ) for camera_info in cameras_info
+    )
 
 class ElektronnyGorogCamera(Camera):
     def __init__(
         self,
         coordinator: ElektronnyGorogDataUpdateCoordinator,
-        camera: dict,
-        stream_url: str
+        camera_info: dict
     ) -> None:
-        LOGGER.info("ElektronnyGorogCamera init %s", camera)
+        LOGGER.info("ElektronnyGorogCamera init %s", camera_info)
         super().__init__()
         self._coordinator: ElektronnyGorogDataUpdateCoordinator = coordinator
-        self._camera_info: dict = camera
+        self._camera_info: dict = camera_info
         self._id = self._camera_info["ID"]
         self._name = self._camera_info["Name"]
         self._is_on = self._camera_info["IsActive"] == 1
         self._is_streaming = self._camera_info["State"] == 1
         self._is_recording = self._camera_info["RecordType"] == 1
         self._attr_supported_features = CameraEntityFeature.STREAM
-        self._stream_url: str | None = stream_url
+        self._stream_url: str | None = None
         self._image: bytes | None = None
 
     @property
@@ -93,9 +82,13 @@ class ElektronnyGorogCamera(Camera):
 
     async def stream_source(self) -> str | None:
         """Return the source of the stream."""
+        self._stream_url = await self._coordinator.get_camera_stream(self._id)
+        LOGGER.info(f"Stream url is {self._stream_url}")
         return self._stream_url
 
     async def async_update(self) -> None:
         """Update camera state."""
         self._camera_info = await self._coordinator.update_camera_state(self._id)
-        self._stream_url = await self._coordinator.get_camera_stream(self._id)
+        self._is_on = self._camera_info["IsActive"] == 1
+        self._is_streaming = self._camera_info["State"] == 1
+        self._is_recording = self._camera_info["RecordType"] == 1
