6dbf5e774333a91cced9051366950a943ebbe734
Vyacheslav Scherbinin <gentslava@mail.ru>
2025-03-30 23:33:34 +0700

    Code style

--- custom_components/elektronny_gorod/camera.py
+++ custom_components/elektronny_gorod/camera.py
@@ -3,7 +3,12 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import CONF_HEIGHT, CONF_WIDTH, DOMAIN, LOGGER
+from .const import (
+  DOMAIN,
+  LOGGER,
+  CONF_WIDTH,
+  CONF_HEIGHT,
+)
 from .coordinator import ElektronnyGorodUpdateCoordinator
 
 async def async_setup_entry(
