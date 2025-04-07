64cd6cacc3bfa4a1de47e5fb3216dc9a114413be
Vyacheslav Shcherbinin <gentslava@mail.ru>
2025-04-07 17:05:54 +0700

    Hash password timestamp

--- custom_components/elektronny_gorod/helpers.py
+++ custom_components/elektronny_gorod/helpers.py
@@ -12,8 +12,6 @@ def find(items: list, condition: Callable) -> object:
             return item
     return None
 
-def get_timestamp() -> str:
-    return f"{datetime.now().isoformat()[:-3]}Z"
 
 def hash_password(password: str) -> str:
     password_bytes = password.encode('utf-8')
@@ -21,8 +19,10 @@ def hash_password(password: str) -> str:
     base64_encoded = base64.b64encode(sha1_hash).decode('utf-8')
     return base64_encoded
 
-def hash_password_timestamp(password: str, timestamp: str) -> str:
-    password_bytes = password.encode('utf-8')
-    combo = password_bytes + timestamp.encode('utf-8')
-    md5_encoded = hashlib.md5(combo).hexdigest()
+
+def hash_password_timestamp(login: str, password: str, time: str) -> str:
+    prefix = "DigitalHomeNTKpassword"
+    secret = "789sdgHJs678wertv34712376"
+    raw_string = f"{prefix}{login}{password}{time}{secret}"
+    md5_encoded = hashlib.md5(raw_string.encode()).hexdigest()
     return md5_encoded
